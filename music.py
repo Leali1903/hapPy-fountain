ce7a21b1d8c790c3a4e62aa761e71445cf5a7f38
Leali1903 <45393864+Leali1903@users.noreply.github.com>
2019-01-19 17:53:27 +0100

    running with local files

--- music.py
+++ music.py
@@ -5,46 +5,31 @@ pygame.mixer.init()
 
 import os
 
-def find_tracks(folder):
+def find_play_tracks(folder):
     for root, dirs, files in os.walk(folder):  
         for tracks in files:
             if tracks.endswith(".mp3"):
                 print(tracks)
-            else: print('No mp3-files found')
+                pygame.mixer.music.load(folder + tracks)
+                pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy() == True:
+                continue
     return tracks
-def play_tracks(folder):
-    for track in tracks: 
-        pygame.mixer.music.load(folder + track)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            continue
-    
-    
 
-eye_input = 'happy'
+eye_input = 'sad'
 
 if eye_input == 'happy': # in case eyetracking input was happy
     folder = "/home/pi/Music/happy/"
-    tracks = find_tracks(folder)
-    play_tracks(folder)
+    tracks = find_play_tracks(folder)
 elif eye_input == 'chillen': # in case eyetracking input was chillen
-    find_tracks("/home/pi/Music/chillen")
-    pygame.mixer.music.load(tracks)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    folder = "/home/pi/Music/chillen/"
+    tracks = find_play_tracks(folder)
 elif eye_input == 'party': # in case eyetracking input was party
-    find_tracks("/home/pi/Music/party")
-    pygame.mixer.music.load(tracks)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    folder = "/home/pi/Music/party/"
+    tracks = find_play_tracks(folder)
 elif eye_input == 'sad': # in case eyetracking input was sad
-    pfind_tracks("/home/pi/Music/sad")
-    pygame.mixer.music.load(tracks)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
+    folder = "/home/pi/Music/sad/"
+    tracks = find_play_tracks(folder)
 else:
     print('Error: No Input received!')
 
