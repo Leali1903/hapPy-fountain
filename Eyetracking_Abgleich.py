691b743e87696fd7855504fc5f8c674a13e6c93d
Lea <lea.dingler@gmail.com>
2019-01-20 16:23:20 +0100

    Anfang Eyetracking_Abgleich

--- Eyetracking_Abgleich.py
+++ Eyetracking_Abgleich.py
@@ -1,30 +1,44 @@
 import math
+import numpy as np
 
-x = 5
-y = 2
-z = 1
+# data_sample = [[x, y, z], [x, y, z], [x, y, z]]
+# time_movement_start = 0 # = Startzeitpunkt der Bewegung
+# time_movement_end = 1 # = Endzeitpunkt der Bewegung
 
 
-data_sample = [[x, y, z], [x, y, z], [x, y, z]]
-time_movement_start = 0 # = Startzeitpunkt der Bewegung
-time_movement_end = 1 # = Endzeitpunkt der Bewegung
-
-while true:
+def extract_samples(data):
+    eye_movement_samples = []
     for sample in data_sample:
-        if sample[2] == time_movement_start:
-            while sample[2] != time_movement_end:
-                movement_samples = []
-                movement_samples = movement_samples.append(data_sample[sample])
+        if sample[0] == time_movement_start:
+            while sample[0] != time_movement_end:
+                eye_movement_samples = eye_movement_samples.append(sample)
+    return eye_movement_samples
 
 
-    movement_x= data_sample[0][0] - data_sample[-1][0]
-    movement_y = data_sample[0][1] - data_sample[-1][1]
-    movement_xy = math.sqrt(movement_x**2 + movement_y**2)
-    angle = math.acos(movement_y/movement_xy)
+def correlation(x, y):
+    sd_x = np.sqrt(np.var(x))
+    sd_y = np.sqrt(np.var(y))
+    r = np.cov(x,y)/(sd_x * sd_y)
+    return r
 
 
-    if angle == angle_happy
+while GUI == 'on':
+    extract_samples(data_sample)
+    eye_movement_x = eye_movement_sample[0][0] - eye_movement_sample[-1][0]
+    eye_movement_y = eye_movement_sample[0][1] - eye_movement_sample[-1][1]
+    movement_xy = math.sqrt(eye_movement_x**2 + eye_movement_y**2)
 
+    eye_x = eye_movement_samples[:][0]
+    eye_y = eye_movement_samples[:][1]
+
+    if correlation(eye_x, gui_x_happy) >= 0.7 & eye_movement_x > 0 & eye_movement_y > 0:
+        eye_input == 'happy'
+    elif correlation(eye_x, gui_x_sad) >= 0.7 & eye_movement_x < 0 & eye_movement_y > 0:
+        eye_input == 'sad'
+    elif correlation(eye_x, gui_x_chillen) >= 0.7 & eye_movement_x > 0 & eye_movement_y < 0:
+        eye_input == 'chillen'
+    elif correlation(eye_x, gui_x_party) >= 0.7 & eye_movement_x < 0 & eye_movement_y < 0:
+        eye_input == 'party'
 
 
 
