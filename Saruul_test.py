87bebd0d580b533dacf0e5a436ae70ab77343384
Leali1903 <45393864+Leali1903@users.noreply.github.com>
2019-01-22 23:11:08 +0100

    Create Saruul_test.py

---
+++ Saruul_test.py
@@ -0,0 +1,34 @@
+# Funktion zum suchen und abspielen der Tracks
+def find_play_tracks(folder):
+    tracks = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(".mp3"):
+                print(file)
+                tracks.append(file)
+    return tracks
+    
+    
+ 
+    
+
+###################### STEUERUNG JE NACH INPUT ######################
+
+# Einschalten des pygame-mixers für die Musik
+pygame.mixer.init()
+
+# Inputschleife
+if eye_input == 'happy':
+    ### BRUNNEN ###
+    os.system(ON)
+    time.sleep(60)  # rausnehmen weil nach Liedern ausgeschaltet
+    os.system(OFF)
+    ### MUSIK ###
+    folder = "/home/pi/Music/happy/"
+    tracks = find_play_tracks(folder)
+    for track in tracks:
+        pygame.mixer.music.queue(track)
+    
+    # ### LED LICHTERKETTE ###
+    while True:  # weil es sich immer weiter bewegen soll.
+         cycle(0.001, wheel_color)  # Farbübergänge in bunt in Kreisform
