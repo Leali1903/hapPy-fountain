9c26eef151031b65a04751b3d2892b1cf1000658
Lea <lea.dingler@gmail.com>
2019-01-27 18:46:23 +0100

    Alle Funktionen in einem Skript -- funktioniert jetzt

--- socket/socket_client_PC.py
+++ socket/socket_client_PC.py
@@ -13,18 +13,50 @@
 # print('Received', repr(data))
 
 
+# import socket
+#
+#
+# def receive_data():
+#     HOST = "0.0.0.0"
+#     PORT = 65432
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+#
+#         server.bind((HOST, PORT))
+#         server.listen(1) # soll nur eine Verbindung eingehen
+#         while True:
+#             connection, address = server.accept()
+#             connection.setblocking(False)
+#             eye_input = connection.recv(64)
+#             self.accept_socket.recv(buffer_size)
+#             if len(buf) > 0:
+#                 print(eye_input)
+#                 break
+#
+#
+# receive_data()
+
 import socket
 
 
-def receive_data():
-    HOST = 'localhost'
-    PORT = 65432
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind((HOST, PORT))
-        server.listen(1) # soll nur eine Verbindung eingehen
-        while True:
-            connection, address = server.accept()
-            eye_input = connection.recv(64)
-            if len(buf) > 0:
-                print eye_input
-                break
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
