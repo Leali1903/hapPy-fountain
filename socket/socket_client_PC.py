d3c2ecd2860f607e7171afe21e52ee99ea799233
leali1903 <lea.dingler@gmail.com>
2019-01-27 22:45:50 +0100

    EYE_Tracking

--- socket/socket_client_PC.py
+++ socket/socket_client_PC.py
@@ -63,8 +63,8 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '172.16.107.164'  # The server's hostname or IP address
+PORT = 60002
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
