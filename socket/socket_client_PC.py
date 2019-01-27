122f351bf7760d41368a0172facf1376f382a259
Lea <lea.dingler@gmail.com>
2019-01-27 21:54:57 +0100

    Socket update

--- socket/socket_client_PC.py
+++ socket/socket_client_PC.py
@@ -21,7 +21,7 @@
 #     PORT = 65432
 #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
 #
-#         server.bind((HOST, PORT))
+#         server.bind(('', PORT))
 #         server.listen(1) # soll nur eine Verbindung eingehen
 #         while True:
 #             connection, address = server.accept()
@@ -35,28 +35,40 @@
 #
 # receive_data()
 
+# import socket
+#
+#
+# def client_program():
+#     host = socket.gethostname()  # as both code is running on same pc
+#     port = 5000  # socket server port number
+#
+#     client_socket = socket.socket()  # instantiate
+#     client_socket.connect((host, port))  # connect to the server
+#
+#     message = input(" -> ")  # take input
+#
+#     while message.lower().strip() != 'bye':
+#         client_socket.send(message.encode())  # send message
+#         data = client_socket.recv(1024).decode()  # receive response
+#
+#         print('Received from server: ' + data)  # show in terminal
+#
+#         message = input(" -> ")  # again take input
+#
+#     client_socket.close()  # close the connection
+#
+#
+# if __name__ == '__main__':
+#     client_program()
+
 import socket
 
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
-def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
-
-    message = input(" -> ")  # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
+print('Received', repr(data))
