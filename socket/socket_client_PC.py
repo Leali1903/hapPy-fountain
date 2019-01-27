d7de36ece3c34a17b04acca1e554b8dcb90828fd
Lea <lea.dingler@gmail.com>
2019-01-27 13:05:37 +0100

    Alle Funktionen in einem Skript -- funktioniert jetzt

--- socket/socket_client_PC.py
+++ socket/socket_client_PC.py
@@ -1,13 +1,30 @@
+# import socket
+#
+#
+# # Socket zum Empfang von ET Daten vom PC
+# HOST = ''  # Linas IP address
+# PORT = 65432        # The port used by the server
+#
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall('et_input.json')
+#     data = s.recv(65432)
+#
+# print('Received', repr(data))
+
+
 import socket
 
 
-# Socket zum Empfang von ET Daten vom PC
-HOST = ''  # Linas IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall('et_input.json')
-    data = s.recv(65432)
-
-print('Received', repr(data))
+def receive_data():
+    HOST = 'localhost'
+    PORT = 65432
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((HOST, PORT))
+        server.listen(1) # soll nur eine Verbindung eingehen
+        while True:
+            connection, address = server.accept()
+            eye_input = connection.recv(64)
+            if len(buf) > 0:
+                print eye_input
+                break
